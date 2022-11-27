5c4c5c4bb7afa4029a0dd0dceb51502f5af87662
Harison Gachuru <harisongachuru@gmail.com>
2022-11-27 08:33:35 +0300

    Add ListAdapter

---
+++ app/src/main/java/com/example/inventory/ItemListAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.inventory
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
+import com.example.inventory.databinding.ItemListItemBinding
+
+class ItemListAdapter(private val onItemClicked: (Item) -> Unit) :
+    ListAdapter<Item, ItemListAdapter.ItemListViewHolder>(DiffCallback) {
+
+    class ItemListViewHolder(private var binding: ItemListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Item) {
+            binding.apply {
+                itemName.text = item.itemName
+                itemPrice.text = item.getFormattedPrice()
+                itemQuantity.text = item.quantityInStock.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemListViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemListItemBinding.inflate(layoutInflater, parent, false)
+        return ItemListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemListViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.itemView.setOnClickListener { onItemClicked(item) }
+        holder.bind(item)
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
